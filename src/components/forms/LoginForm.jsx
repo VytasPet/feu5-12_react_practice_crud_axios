0e740c63fe4502b5070756729382dd2fdba91eeb
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-04 12:28:00 +0300

    formik submit

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import { SubmitButton } from '../ui/Button.styled';
+import { useFormik } from 'formik';
 
 function LoginForm() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      console.log('form values ===', values);
+    },
+  });
   return (
-    <div>
-      <form>
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+        <input
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
         <SubmitButton>Login</SubmitButton>
       </form>
-    </div>
+      <div>
+        <p>email: {formik.values.email}</p>
+        <p>password: {formik.values.password}</p>
+      </div>
+    </>
   );
 }
 
