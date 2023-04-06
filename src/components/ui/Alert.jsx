f4315224b2f2cf7b21f091052493680c11d43d77
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-06 16:52:42 +0300

    alert

---
+++ src/components/ui/Alert.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const AlertWrapper = styled.div`
+  position: relative;
+  padding: 0.75rem 1.25rem;
+  margin-bottom: 1rem;
+  border-radius: 0.25rem;
+  background-color: ${({ type }) =>
+    type === 'success' ? '#d4edda' : type === 'danger' ? '#f8d7da' : '#cce5ff'};
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 0.5rem;
+  background-color: transparent;
+  border: none;
+  color: #000;
+  font-size: 1.5rem;
+  cursor: pointer;
+
+  &:hover {
+    color: #ff0000;
+  }
+`;
+
+const Alert = ({ type = 'info', children }) => {
+  const [show, setShow] = useState(true);
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <AlertWrapper type={type}>
+      {children}
+      <CloseButton onClick={() => setShow(false)}>&times;</CloseButton>
+    </AlertWrapper>
+  );
+};
+
+export default Alert;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Container from '../components/ui/Container';
+import Alert from './../components/ui/Alert';
 
 function HomePage() {
   return (
     <Container>
+      <Alert>Normal</Alert>
+      <Alert type={'danger'}>Ivyko klaida</Alert>
+      <Alert type={'success'}>success</Alert>
+
       <h1>HomePage</h1>
       <p>Welcome to HomePage</p>
     </Container>
