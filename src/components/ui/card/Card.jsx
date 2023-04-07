f421e6ad9b6efb4713f8fa3899c4f2c50a2e961f
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-07 10:14:46 +0300

    grid and card styled

--- src/components/ui/card/Card.jsx
+++ src/components/ui/card/Card.jsx
@@ -1,11 +1,25 @@
-import './card.css';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 // perdaryti kad div butu styled componentas
+const Wrap = styled.div`
+  padding: 1rem;
+  box-shadow: 5px 5px 10px rgb(180, 190, 194);
+  border: 1px solid rgb(191, 180, 180);
+  border-radius: 5px;
+  background-color: snow;
+`;
 
 function Card({ children }) {
-  return <div className="card">{children}</div>;
+  return <Wrap>{children}</Wrap>;
 }
 
 // prop types buti vienas elementas arba masyvas elementu
-
+// validuoti props
+Card.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element,
+  ]).isRequired,
+};
 export default Card;
--- src/components/ui/grid/Grid.jsx
+++ src/components/ui/grid/Grid.jsx
@@ -1,20 +1,23 @@
-import './grid.css';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-// Grid
-// pakeisti css i styled
+const StyledGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(${(props) => props.cols}, 1fr);
+  gap: 16px;
+`;
 
 // apjuosiantis elementas suteikiantis papildomu stiliaus ar funkciju dalyku
-function Grid(props) {
-  const inlineStyleObj = {
-    // color: 'tomato',
-    gridTemplateColumns: `repeat(${props.cols}, 1fr)`,
-  };
-  return (
-    <div style={inlineStyleObj} className={'myGrid'}>
-      {props.children}
-    </div>
-  );
+function Grid({ cols = 3, children }) {
+  return <StyledGrid cols={cols}>{children}</StyledGrid>;
 }
-
 // validuoti props
+Grid.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element,
+  ]).isRequired,
+  cols: PropTypes.number,
+};
+
 export default Grid;
